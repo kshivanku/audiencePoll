4344ed37e4fe6cf9f088f47633abdd9c69873c2e
kshivanku <kshivanku@gmail.com>
2017-11-14 00:27:30 -0500

    firebase setup

--- public/sketch.js
+++ public/sketch.js
@@ -19,44 +19,30 @@ firebase.initializeApp(config);
 var database = firebase.database();
 var allusers = database.ref('allusers');
 
-var userkey;
+var username;
 
 $(document).ready(function(){
   loadPage("login");
-})
+  $("#usernameForm").submit(function(event){
+    loadPage("survey_page");
+    username = $("#username").val(); //sets the name of the user as a global variable
+    return false;
+  })
 
-$("#loginForm").submit(function(event){
-  console.log("login form submit button clicked");
-  event.preventDefault();
-  loadPage("survey_page");
-  var username = $("#username").val();
-  // saveToDb(allusers, username, "name");
-})
-
-$("#questionForm").submit(function(event){
-  event.preventDefault();
-  height = $("#height").val();
-  saveToDb(userkey, height, "height");
+  $("#questionForm").submit(function(event){
+    event.preventDefault();
+    answer = $("#question").val();
+    saveToDb(database.ref(username), answer, "question");
+    return false;
+  })
 })
 
 function saveToDb(key, value, dataType) {
-  if(dataType == "name"){
-    var reqbody = {
-      'name' : value
-    }
-    console.log("Saving to db: " + reqbody);
-    var result = key.push(reqbody);
-    userkey = result.key;
-    console.log("userkey: " + userkey);
-  }
-  else if(dataType == "height") {
-    var reqbody = {
-      'height': value
-    }
+    var reqbody = {};
+    reqbody[dataType] = value;
     key.push(reqbody);
   }
 
-}
 
 function loadPage(page){
   switch (page) {
